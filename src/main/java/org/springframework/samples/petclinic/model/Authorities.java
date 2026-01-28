ca53541d494835724061f3c29c8453d6bc1972e1
limtsesh <limtsesh@yahoo.co.uk>
2026-01-28 16:02:27 +0800

    Corrected Maven build errors

--- src/main/java/org/springframework/samples/petclinic/model/Authorities.java
+++ src/main/java/org/springframework/samples/petclinic/model/Authorities.java
@@ -13,4 +13,20 @@ public class Authorities {
 	@Id
 	String username;
 	String authority;
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getAuthority() {
+		return this.authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
 }
--- src/main/java/org/springframework/samples/petclinic/model/User.java
+++ src/main/java/org/springframework/samples/petclinic/model/User.java
@@ -16,4 +16,28 @@ public class User{
 	String password;
 	
 	boolean enabled;
+    
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
